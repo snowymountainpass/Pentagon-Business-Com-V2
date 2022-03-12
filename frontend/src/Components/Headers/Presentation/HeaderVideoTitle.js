c5ca4cffbc3075d594f01d24f87fa33c5d672ee7
snowymountainpass <73165724+snowymountainpass@users.noreply.github.com>
2022-03-12 12:24:29 +0200

    improved landing page video display appearance

--- frontend/src/Components/Headers/Presentation/HeaderVideoTitle.js
+++ frontend/src/Components/Headers/Presentation/HeaderVideoTitle.js
@@ -2,51 +2,36 @@ import React from "react";
 import PropTypes from "prop-types";
 
 // components
-import Button from "../../Elements/Button";
-import Skew from "../../Skews/Skew";
-import {toPadding} from "chart.js/helpers";
+// import Button from "../../Elements/Button";
+// import Skew from "../../Skews/Skew";
+// import {toPadding} from "chart.js/helpers";
 //75
 export default function HeaderVideoTitle({
   video,
-  title,
-  subtitle,
-  skew,
+
 }) {
   return (
     <>
-      <br/>
-      <br/>
-      <br/>
-      <div className="p-5 flex items-center overflow-hidden relative bg-black min-h-screen-75">
-        <div className="absolute w-full bg-indigo-500 h-full z-1 opacity-30 top-0 left-0"></div>
+
+      <div>
+        <br/>
+        <br/>
+        <br/>
+      </div>
+      {/*<div className="p-5 flex items-center overflow-hidden relative bg-black min-h-screen-75 min-h-700-px">*/}
+      <div className="flex position-relative bg-black min-h-700-px">
+
         <video
           playsInline="playsinline"
           autoPlay="autoplay"
           muted="muted"
           loop="loop"
-          className="absolute min-w-full max-w-none min-h-full z-2 transform -translate-x-1/2 -translate-y-1/2 top-7/20 left-1/2 mt-xl-5"
+          // className="absolute min-w-full max-w-none min-h-full z-2 transform -translate-x-1/2 -translate-y-1/2 top-7/20 left-1/2 mt-xl-5"
+          className="position-relative min-w-full max-w-none min-h-screen-60" //min-h-screen min-h-full
         >
           <source src={video} type="video/mp4" />
         </video>
-        <div className="container mx-auto px-4">
-          <div className="relative text-center flex flex-wrap -mx-4 z-1">
-            <div className="mx-auto px-4 relative w-full lg:w-8/12">
-              <h1 className="text-4xl font-semibold leading-tight text-white">
-                {title}
-              </h1>
-              <h4 className="mt-2 text-lg text-white opacity-75 mb-8">
-                {subtitle}
-              </h4>
-              {/*<Button color={buttonColor} size="lg">*/}
-              {/*  <i className={buttonIcon + " mr-2"}></i>*/}
-              {/*  {buttonText}*/}
-              {/*</Button>*/}
-            </div>
-          </div>
-        </div>
-        <div className="w-full absolute text-white bottom-0 z-1">
-          <Skew {...skew} />
-        </div>
+
       </div>
     </>
   );
@@ -58,24 +43,5 @@ HeaderVideoTitle.defaultProps = {
 
 HeaderVideoTitle.propTypes = {
   video: PropTypes.string.isRequired,
-  title: PropTypes.string.isRequired,
-  subtitle: PropTypes.string.isRequired,
-  buttonText: PropTypes.string.isRequired,
-  buttonIcon: PropTypes.string,
-  buttonColor: PropTypes.oneOf([
-    "light",
-    "dark",
-    "blueGray",
-    "red",
-    "orange",
-    "amber",
-    "emerald",
-    "teal",
-    "lightBlue",
-    "indigo",
-    "purple",
-    "pink",
-  ]),
-  // props to pass to the Skew component
-  skew: PropTypes.object,
+
 };
--- frontend/src/Texts/Presentation/headers/headervideotitle.js
+++ frontend/src/Texts/Presentation/headers/headervideotitle.js
@@ -1,15 +1,5 @@
 const textProps = {
   video: require("../../../Assets/img/Pentagon Assets/Alcatel-Lucent-Header-Video.mp4"),
 
-
-  title: "",
-  subtitle:
-    "",
-  buttonColor: "red",
-  buttonText: "Get in contact",
-  buttonIcon: "fas fa-play",
-  skew: {
-    color: "white",
-  },
 };
 export default textProps;
