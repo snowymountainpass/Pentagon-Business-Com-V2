30c03fe0ffc3f310f9f4abbe39340e763a5213ee
snowymountainpass <73165724+snowymountainpass@users.noreply.github.com>
2022-03-14 19:38:24 +0200

    added columns to Product Category table

--- src/main/java/com/clockworkcode/pentagonbusinesscomv2/model/product/Product_Category.java
+++ src/main/java/com/clockworkcode/pentagonbusinesscomv2/model/product/Product_Category.java
@@ -1,4 +1,32 @@
 package com.clockworkcode.pentagonbusinesscomv2.model.product;
 
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.sql.Timestamp;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+
+@Entity(name = "ProductCategory") //singular !!
+@Table(name = "ProductCategories") //plural !!
 public class Product_Category {
+
+
+    @Id
+    @SequenceGenerator(name ="productcategory_sequence" ,sequenceName ="productcategory_sequence" ,allocationSize = 1)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "productcategory_sequence")
+    private Long productCategoryID;
+
+    private String productCategoryName;
+    private String productCategoryDescription;
+    private Timestamp createdAt;
+    private Timestamp modifiedAt;
+    private Timestamp deletedAt;
 }
