2c39373b55b131eaae72a4627c958d599c446c53
snowymountainpass <73165724+snowymountainpass@users.noreply.github.com>
2022-03-14 19:40:57 +0200

    added columns to Product Inventory table

--- src/main/java/com/clockworkcode/pentagonbusinesscomv2/model/product/Product_Inventory.java
+++ src/main/java/com/clockworkcode/pentagonbusinesscomv2/model/product/Product_Inventory.java
@@ -1,4 +1,31 @@
 package com.clockworkcode.pentagonbusinesscomv2.model.product;
 
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.sql.Timestamp;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+
+@Entity(name = "ProductInventory") //singular !!
+@Table(name = "ProductInventories") //plural !!
 public class Product_Inventory {
+
+    @Id
+    @SequenceGenerator(name ="productinventory_sequence" ,sequenceName ="productinventory_sequence" ,allocationSize = 1)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "productinventory_sequence")
+    private Long productInventoryID;
+
+    private Integer quantity;
+    private Timestamp createdAt;
+    private Timestamp modifiedAt;
+    private Timestamp deletedAt;
+
 }
