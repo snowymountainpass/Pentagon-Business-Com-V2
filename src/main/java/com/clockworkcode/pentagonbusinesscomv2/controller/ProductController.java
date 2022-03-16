b8acb0e914f53cb11859b698f38167d17b8c44ee
snowymountainpass <73165724+snowymountainpass@users.noreply.github.com>
2022-03-16 12:30:51 +0200

    updated methods in Product Controller

--- src/main/java/com/clockworkcode/pentagonbusinesscomv2/controller/ProductController.java
+++ src/main/java/com/clockworkcode/pentagonbusinesscomv2/controller/ProductController.java
@@ -1,6 +1,7 @@
 package com.clockworkcode.pentagonbusinesscomv2.controller;
 
-import com.clockworkcode.pentagonbusinesscomv2.model.Product;
+import com.clockworkcode.pentagonbusinesscomv2.model.product.Product;
+import com.clockworkcode.pentagonbusinesscomv2.service.ProductDBService;
 import com.clockworkcode.pentagonbusinesscomv2.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -16,37 +17,37 @@ import java.util.stream.Collectors;
 public class ProductController {
 
     @Autowired
-    ProductService productService;
+    ProductDBService productDBService;
 
     @GetMapping()
     public List<Product> getAllProducts() {
 
 //        productService.getAllProducts().forEach(product -> System.out.println(product.getProductName()));
 
-        return productService.getAllProducts();
+        return productDBService.getAllProducts();
     }
 
     @GetMapping("/product/{productName}")
     public ResponseEntity<List<Product>> getProductsByName(@PathVariable String productName) {
 
-        System.out.println("productName: " + productName);
-
+//        System.out.println("productName: " + productName);
 //        System.out.println("No. of products with this name: "+productService.getProductsByName(productName).size());
+//        productService.getProductsByName(productName.toLowerCase()).forEach(product1 -> System.out.println(product1.getProductName()));
 
-        List<Product> listOfProducts = productService.getProductsByName(productName.toLowerCase());
-//               productService.getProductsByName(productName.toLowerCase()).forEach(product1 -> System.out.println(product1.getProductName()));
+//        List<Product> listOfProducts = productService.getProductsByName(productName.toLowerCase());
+        List<Product> listOfProducts = productDBService.getProductsByName(productName.toLowerCase());
 
         return ResponseEntity.ok().body(listOfProducts);
 
     }
 
-
     @GetMapping("/{productCategory}")
     public ResponseEntity<List<Product>>getProductsByCategory(@PathVariable String productCategory) {
 
-        System.out.println("SELECTED Product Category: "+ productCategory);
+//        System.out.println("SELECTED Product Category: "+ productCategory);
 
-        return ResponseEntity.ok().body( productService.getAllProductsByCategory(productCategory) );
+//        return ResponseEntity.ok().body( productDBService.getAllProductsByCategory(productCategory) );
+        return ResponseEntity.ok().body( productDBService.getProductsByProductCategoryName(productCategory) );
 
     }
 
@@ -55,33 +56,38 @@ public class ProductController {
 
         List<Product> productsFromBrands = new ArrayList<>();
 
-        brandsList.forEach(s -> productsFromBrands.addAll(getAllProducts().stream().filter(product -> product.getBrand().equals(s)).collect(Collectors.toList())));
+//        brandsList.forEach(s -> productsFromBrands.addAll(getAllProducts().stream().filter(product -> product.getBrand().equals(s)).collect(Collectors.toList())));
+        brandsList
+                .forEach(s -> productsFromBrands
+                        .addAll(getAllProducts().stream()
+                                .filter(product -> product.getProductBrand().getProductBrandName().equals(s))
+                                .collect(Collectors.toList())));
 
         return ResponseEntity.ok().body(productsFromBrands);
     }
-
-    @GetMapping("/min-price-only")
-    public  ResponseEntity<List<Product>> getProductsAboveMinPrice(@RequestParam Integer minPrice){
-
-        return ResponseEntity.ok().body( productService.getAllProducts().stream().filter(product -> product.getPrice()>=minPrice).collect(Collectors.toList()) );
-    }
-
-    @GetMapping("/min-max-prices")
-    public  ResponseEntity<List<Product>> getProductsAboveMinPrice(@RequestParam Integer minPrice,@RequestParam Integer maxPrice) {
-
-        return ResponseEntity.ok().body( productService.getAllProducts().stream().filter(product -> product.getPrice()>=minPrice && product.getPrice()<=maxPrice).collect(Collectors.toList()) );
-    }
-
-
-    @GetMapping("/price-descending")
-    public List<Product> getProductsPriceDecreasing() {
-
-        return productService.getProductsPriceDecreasing();
-    }
-
-    @GetMapping("/price-ascending")
-    public List<Product> getProductsPriceIncreasing() {
-        return productService.getProductsPriceIncreasing();
-    }
+//
+//    @GetMapping("/min-price-only")
+//    public  ResponseEntity<List<Product>> getProductsAboveMinPrice(@RequestParam Integer minPrice){
+//
+//        return ResponseEntity.ok().body( productService.getAllProducts().stream().filter(product -> product.getPrice()>=minPrice).collect(Collectors.toList()) );
+//    }
+//
+//    @GetMapping("/min-max-prices")
+//    public  ResponseEntity<List<Product>> getProductsAboveMinPrice(@RequestParam Integer minPrice,@RequestParam Integer maxPrice) {
+//
+//        return ResponseEntity.ok().body( productService.getAllProducts().stream().filter(product -> product.getPrice()>=minPrice && product.getPrice()<=maxPrice).collect(Collectors.toList()) );
+//    }
+//
+//
+//    @GetMapping("/price-descending")
+//    public List<Product> getProductsPriceDecreasing() {
+//
+//        return productService.getProductsPriceDecreasing();
+//    }
+//
+//    @GetMapping("/price-ascending")
+//    public List<Product> getProductsPriceIncreasing() {
+//        return productService.getProductsPriceIncreasing();
+//    }
 
 }
---
+++ src/main/java/com/clockworkcode/pentagonbusinesscomv2/controller/ProductControllerLocal.java
@@ -0,0 +1,87 @@
+//package com.clockworkcode.pentagonbusinesscomv2.controller;
+//
+//import com.clockworkcode.pentagonbusinesscomv2.model.Product;
+//import com.clockworkcode.pentagonbusinesscomv2.service.ProductService;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.http.ResponseEntity;
+//import org.springframework.web.bind.annotation.*;
+//
+//import java.util.ArrayList;
+//import java.util.List;
+//import java.util.stream.Collectors;
+//
+//@RestController
+//@RequestMapping("/e-shop")
+//@CrossOrigin(origins = "http://localhost:3000")
+//public class ProductControllerLocal {
+//
+//    @Autowired
+//    ProductService productService;
+//
+//    @GetMapping()
+//    public List<Product> getAllProducts() {
+//
+////        productService.getAllProducts().forEach(product -> System.out.println(product.getProductName()));
+//
+//        return productService.getAllProducts();
+//    }
+//
+//    @GetMapping("/product/{productName}")
+//    public ResponseEntity<List<Product>> getProductsByName(@PathVariable String productName) {
+//
+//        System.out.println("productName: " + productName);
+//
+////        System.out.println("No. of products with this name: "+productService.getProductsByName(productName).size());
+//
+//        List<Product> listOfProducts = productService.getProductsByName(productName.toLowerCase());
+////               productService.getProductsByName(productName.toLowerCase()).forEach(product1 -> System.out.println(product1.getProductName()));
+//
+//        return ResponseEntity.ok().body(listOfProducts);
+//
+//    }
+//
+//
+//    @GetMapping("/{productCategory}")
+//    public ResponseEntity<List<Product>>getProductsByCategory(@PathVariable String productCategory) {
+//
+//        System.out.println("SELECTED Product Category: "+ productCategory);
+//
+//        return ResponseEntity.ok().body( productService.getAllProductsByCategory(productCategory) );
+//
+//    }
+//
+//    @PostMapping("/brands")
+//    public ResponseEntity<List<Product>> getProductsByBrands(@RequestBody List<String> brandsList){
+//
+//        List<Product> productsFromBrands = new ArrayList<>();
+//
+//        brandsList.forEach(s -> productsFromBrands.addAll(getAllProducts().stream().filter(product -> product.getBrand().equals(s)).collect(Collectors.toList())));
+//
+//        return ResponseEntity.ok().body(productsFromBrands);
+//    }
+//
+//    @GetMapping("/min-price-only")
+//    public  ResponseEntity<List<Product>> getProductsAboveMinPrice(@RequestParam Integer minPrice){
+//
+//        return ResponseEntity.ok().body( productService.getAllProducts().stream().filter(product -> product.getPrice()>=minPrice).collect(Collectors.toList()) );
+//    }
+//
+//    @GetMapping("/min-max-prices")
+//    public  ResponseEntity<List<Product>> getProductsAboveMinPrice(@RequestParam Integer minPrice,@RequestParam Integer maxPrice) {
+//
+//        return ResponseEntity.ok().body( productService.getAllProducts().stream().filter(product -> product.getPrice()>=minPrice && product.getPrice()<=maxPrice).collect(Collectors.toList()) );
+//    }
+//
+//
+//    @GetMapping("/price-descending")
+//    public List<Product> getProductsPriceDecreasing() {
+//
+//        return productService.getProductsPriceDecreasing();
+//    }
+//
+//    @GetMapping("/price-ascending")
+//    public List<Product> getProductsPriceIncreasing() {
+//        return productService.getProductsPriceIncreasing();
+//    }
+//
+//}
