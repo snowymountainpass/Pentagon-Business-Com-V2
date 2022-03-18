f165301c90828cb0fd9c617ed2bb25f8eb569029
snowymountainpass <73165724+snowymountainpass@users.noreply.github.com>
2022-03-18 07:23:38 +0200

    fixed filtering by product name

--- src/main/java/com/clockworkcode/pentagonbusinesscomv2/controller/ProductController.java
+++ src/main/java/com/clockworkcode/pentagonbusinesscomv2/controller/ProductController.java
@@ -41,11 +41,14 @@ public class ProductController {
 //        List<Product> listOfProducts = productDBService.getProductsByName(productName.toLowerCase());
         List<Product> listOfProducts = productDBService.getAllProducts()
                 .stream()
-                .filter(product -> product.getProductName().equals( productName.toLowerCase() ) ||
-                        product.getProductBrand().getProductBrandName().equals( productName.toLowerCase() ) ||
-                        product.getProductCategory().getProductCategoryName().equals( productName.toLowerCase() )
+                .filter(product -> product.getProductName().toLowerCase().contains(productName.toLowerCase())
                 )
                 .collect(Collectors.toList());
+
+//                .filter(product -> product.getProductName().contains( productName.toLowerCase() ) ||
+//                product.getProductBrand().getProductBrandName().contains( productName.toLowerCase() ) ||
+//                product.getProductCategory().getProductCategoryName().contains( productName.toLowerCase() )
+//        )
 
         return ResponseEntity.ok().body(listOfProducts);
 
