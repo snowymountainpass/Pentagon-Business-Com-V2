c6b1e29162d98ea540406104a260550461cf8798
snowymountainpass <73165724+snowymountainpass@users.noreply.github.com>
2022-03-28 10:59:38 +0300

    added methods to ConfirmationTokenService
    saving and returning a token

--- src/main/java/com/clockworkcode/pentagonbusinesscomv2/service/ConfirmationTokenService.java
+++ src/main/java/com/clockworkcode/pentagonbusinesscomv2/service/ConfirmationTokenService.java
@@ -1,13 +1,23 @@
 package com.clockworkcode.pentagonbusinesscomv2.service;
 
 import com.clockworkcode.pentagonbusinesscomv2.repository.ConfirmationTokenRepository;
+import com.clockworkcode.pentagonbusinesscomv2.security.token.ConfirmationToken;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 @AllArgsConstructor
 public class ConfirmationTokenService {
 
     private final ConfirmationTokenRepository confirmationTokenRepository;
+
+    public void saveConfirmationToken(ConfirmationToken token){
+        confirmationTokenRepository.save(token);
+    }
+    public Optional<ConfirmationToken> getToken(String token){
+       return confirmationTokenRepository.findByToken(token);
+    }
 
 }
