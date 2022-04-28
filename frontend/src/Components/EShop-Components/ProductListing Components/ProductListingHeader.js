7b34a84bd1d024f5c54f7b8a8bf3c3058608d747
snowymountainpass <73165724+snowymountainpass@users.noreply.github.com>
2022-04-28 10:10:13 +0300

    commented out sorting selectbox and page layout switcher

--- frontend/src/Components/EShop-Components/ProductListing Components/ProductListingHeader.js
+++ frontend/src/Components/EShop-Components/ProductListing Components/ProductListingHeader.js
@@ -18,20 +18,20 @@ export default function ProductListingHeader({products,setChangeQuery}) {
                 <div className="form-inline">
                     <span className="mr-md-auto">{products.length} Item(s) found </span>
 
-                    <select className="mr-2 form-inline" data-width="auto">
-                        <option>Price ↑</option>
-                        <option>Price ↓</option>
-                        <option>Best Sellers</option>
-                        <option>Name (A-Z)</option>
-                        <option>Name (Z-A)</option>
-                    </select>
-                    <div className="btn-group">
-                        <a href="#" className="btn btn-outline-secondary active" data-toggle="tooltip"
-                           title="List view">
-                            <i className="fa fa-bars"></i></a>
-                        <a href="#" className="btn  btn-outline-secondary" data-toggle="tooltip" title="Grid view">
-                            <i className="fa fa-th"></i></a>
-                    </div>
+                    {/*<select className="mr-2 form-inline" data-width="auto">*/}
+                    {/*    <option>Price ↑</option>*/}
+                    {/*    <option>Price ↓</option>*/}
+                    {/*    <option>Best Sellers</option>*/}
+                    {/*    <option>Name (A-Z)</option>*/}
+                    {/*    <option>Name (Z-A)</option>*/}
+                    {/*</select>*/}
+                    {/*<div className="btn-group">*/}
+                    {/*    <a href="#" className="btn btn-outline-secondary active" data-toggle="tooltip"*/}
+                    {/*       title="List view">*/}
+                    {/*        <i className="fa fa-bars"></i></a>*/}
+                    {/*    <a href="#" className="btn  btn-outline-secondary" data-toggle="tooltip" title="Grid view">*/}
+                    {/*        <i className="fa fa-th"></i></a>*/}
+                    {/*</div>*/}
 
                 </div>
             </header>
