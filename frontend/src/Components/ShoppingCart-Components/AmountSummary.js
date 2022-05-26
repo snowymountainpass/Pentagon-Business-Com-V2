7eed194f38c541b73428cf1ed9fed14a84c3734d
snowymountainpass <73165724+snowymountainpass@users.noreply.github.com>
2022-05-26 11:23:32 +0300

    Shopping cart page does not show NaN if no items in cart

--- frontend/src/Components/ShoppingCart-Components/AmountSummary.js
+++ frontend/src/Components/ShoppingCart-Components/AmountSummary.js
@@ -5,15 +5,15 @@ export default function AmountSummary({totalAmount,discount}){
             <div className="card-body">
                 <dl className="dlist-align">
                     <dt>Total price:</dt>
-                    <dd className="text-right">EUR {totalAmount}</dd>
+                    <dd className="text-right">EUR {totalAmount?totalAmount:""}</dd>
                 </dl>
                 <dl className="dlist-align">
                     <dt>Discount:</dt>
-                    <dd className="text-right">EUR {discount}</dd>
+                    <dd className="text-right">EUR {discount?discount:""}</dd>
                 </dl>
                 <dl className="dlist-align">
                     <dt>Total:</dt>
-                    <dd className="text-right  h5"><strong>€{totalAmount-discount}</strong></dd>
+                    <dd className="text-right  h5"><strong>€{totalAmount && discount ? totalAmount-discount : ""}</strong></dd>
                 </dl>
                 <hr/>
             </div>
