86e2bc08151a9f1d0dc7c9d5bd5fad3447b15c3c
snowymountainpass <73165724+snowymountainpass@users.noreply.github.com>
2022-06-28 09:58:46 +0300

    removed unused code from ProductListingHeader

--- frontend/src/Components/EShop-Components/ProductListing Components/ProductListingHeader.js
+++ frontend/src/Components/EShop-Components/ProductListing Components/ProductListingHeader.js
@@ -17,22 +17,6 @@ export default function ProductListingHeader({products,setChangeQuery}) {
             <header className="border-bottom mb-4 pb-3">
                 <div className="form-inline">
                     <span className="mr-md-auto">{products.length} Item(s) found </span>
-
-                    {/*<select className="mr-2 form-inline" data-width="auto">*/}
-                    {/*    <option>Price ↑</option>*/}
-                    {/*    <option>Price ↓</option>*/}
-                    {/*    <option>Best Sellers</option>*/}
-                    {/*    <option>Name (A-Z)</option>*/}
-                    {/*    <option>Name (Z-A)</option>*/}
-                    {/*</select>*/}
-                    {/*<div className="btn-group">*/}
-                    {/*    <a href="#" className="btn btn-outline-secondary active" data-toggle="tooltip"*/}
-                    {/*       title="List view">*/}
-                    {/*        <i className="fa fa-bars"></i></a>*/}
-                    {/*    <a href="#" className="btn  btn-outline-secondary" data-toggle="tooltip" title="Grid view">*/}
-                    {/*        <i className="fa fa-th"></i></a>*/}
-                    {/*</div>*/}
-
                 </div>
             </header>
 
@@ -41,7 +25,6 @@ export default function ProductListingHeader({products,setChangeQuery}) {
                     <input
                         id = 'searchBar'
                         type="text"
-                        // className="form-control rounded-left" //old version
                         className="form-control rounded"
                         placeholder="Search"
                         onChange={event => searchInputChange(event)}
